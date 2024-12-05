2566dbdf16c5374937d1394ba28de339e4fe0d5e
Youngjoon Park <pyj2025@gmail.com>
2024-12-04 20:45:01 -0800

    update layout design

--- src/app/namecard/[id]/page.tsx
+++ src/app/namecard/[id]/page.tsx
@@ -1,5 +1,19 @@
 import NameCardClient from '../../../components/NameCardClient';
 
-export default function NameCardPage() {
-  return <NameCardClient />;
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function NameCardPage({ params }: PageProps) {
+  const { id } = await Promise.resolve(params);
+
+  return (
+    <div className="min-h-screen p-4 bg-gray-50">
+      <div className="max-w-2xl mx-auto mt-10">
+        <NameCardClient id={id} />
+      </div>
+    </div>
+  );
 }
